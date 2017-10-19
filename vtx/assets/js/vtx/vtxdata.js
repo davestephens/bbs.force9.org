818b2a304d648960296d07ef265bc35f2121f881
David Stephens <dave@force9.org>
2017-10-20 00:51:30 +0100

    Mega website update

---
+++ vtx/assets/js/vtx/vtxdata.js
@@ -0,0 +1,18 @@
+var vtxdata = {
+	sysName: "fORCE9",
+	wsConnect: "wss://bbs.force9.org:8811",
+	term: "ansi-bbs",
+	codePage: "CP437",
+	fontName: "UVGA16",
+	fontSize: "24px",
+	crtCols: 80,
+	crtRows: 25,
+	crtHistory: 500,
+	xScale: 1,
+	initStr: "",
+	defPageAttr: 0x1010,
+	defCrsrAttr: 0x0207,
+	defCellAttr: 0x0007,
+	telnet: 1,
+	autoConnect: 0
+};
